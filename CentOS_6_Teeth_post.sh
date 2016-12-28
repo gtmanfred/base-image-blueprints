04d7d0f39efcfc6f8189fcec23c34843ed396e7c
shay6739 <shayne.hardesty@rackspace.com>
2016-12-28 17:13:25 -0500

    Don't remove only kernel, as lots of stuff breaks

--- CentOS_6_Teeth_post.sh
+++ CentOS_6_Teeth_post.sh
@@ -15,7 +15,6 @@ REMOVEKERNEL=$(rpm -q kernel)
 wget http://KICK_HOST/packages/centos/6/kernel-2.6.32-504.30.3.el6.x86_64.rpm
 wget http://KICK_HOST/packages/centos/6/kernel-headers-2.6.32-504.30.3.el6.x86_64.rpm
 yum -y localinstall kernel*
-yum -y erase $REMOVEKERNEL
 echo "exclude=kernel*" >> /etc/yum.conf
 
 # update all
